eb81e9578d93c4f88ba72a7b5b55ab29e33c98bf
Pavel Krigin <pavel.krigin.85@gmail.com>
2024-02-01 22:44:46 +0300

    Initial Commit

---
+++ Bookmaker/BookmakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookmakerApp.swift
+//  Bookmaker
+//
+//  Created by Pavel Krigin on 01.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookmakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
